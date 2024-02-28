260d32afa8be558e11f8dcbd8660cca0b70aed2a
Sarah French <sarah.french@hashicorp.com>
2024-02-28 17:00:44 +0000

    Make 'MM upstream' testing of provider functions use central VCSRoot for MM repos. Fix config used with VCR projects for MM testing.

--- .teamcity/components/projects/feature_branches/FEATURE-BRANCH-provider-functions.kt
+++ .teamcity/components/projects/feature_branches/FEATURE-BRANCH-provider-functions.kt
@@ -5,7 +5,7 @@
 
 // This file is controlled by MMv1, any changes made here will be overwritten
 
-package feature_branches
+package projects.feature_branches
 
 import ProviderNameBeta
 import ProviderNameGa
@@ -15,6 +15,8 @@ import generated.PackagesListGa
 import jetbrains.buildServer.configs.kotlin.Project
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import replaceCharsId
+import vcs_roots.ModularMagicianVCSRootBeta
+import vcs_roots.ModularMagicianVCSRootGa
 
 const val featureBranchProviderFunctionsName = "FEATURE-BRANCH-provider-functions"
 
@@ -33,65 +35,45 @@ object HashicorpVCSRootBeta_featureBranchProviderFunctions: GitVcsRoot({
     branchSpec = "" // empty as we'll access no other branches
 })
 
-object ModularMagicianVCSRootGa_featureBranchProviderFunctions: GitVcsRoot({
-    name = "VCS root for the modular-magician/terraform-provider-${ProviderNameGa} repo @ refs/heads/${featureBranchProviderFunctionsName}"
-    url = "https://github.com/modular-magician/terraform-provider-${ProviderNameGa}"
-    branch = "refs/heads/${featureBranchProviderFunctionsName}"
-    branchSpec = "" // empty as we'll access no other branches
-})
-
-object ModularMagicianVCSRootBeta_featureBranchProviderFunctions: GitVcsRoot({
-    name = "VCS root for the modular-magician/terraform-provider-${ProviderNameBeta} repo @ refs/heads/${featureBranchProviderFunctionsName}"
-    url = "https://github.com/modular-magician/terraform-provider-${ProviderNameBeta}"
-    branch = "refs/heads/${featureBranchProviderFunctionsName}"
-    branchSpec = "" // empty as we'll access no other branches
-})
-
 fun featureBranchProviderFunctionSubProject(allConfig: AllContextParameters): Project {
 
     val projectId = replaceCharsId(featureBranchProviderFunctionsName)
 
+    val packageName = "functions" // This project will contain only builds to test this single package
     val sharedResourcesEmpty: List<String> = listOf() // No locking when testing functions
+    val vcrConfig = getVcrAcceptanceTestConfig(allConfig) // Reused below for both MM testing build configs
 
-    var parentId: String = "" // To be overwritten when each build config is generated below.
-    var packageName = "functions"
+    var parentId: String // To be overwritten when each build config is generated below.
 
     // GA
     val gaConfig = getGaAcceptanceTestConfig(allConfig)
-
     // How to make only build configuration to the relevant package(s)
     val functionPackageGa = PackagesListGa.getValue(packageName)
-
     // Enable testing using hashicorp/terraform-provider-google
     parentId = "${projectId}_HC_GA"
     val buildConfigHashiCorpGa = BuildConfigurationForSinglePackage(packageName, functionPackageGa.getValue("path"), "Provider-Defined Functions (GA provider, HashiCorp downstream)", ProviderNameGa, parentId, HashicorpVCSRootGa_featureBranchProviderFunctions, sharedResourcesEmpty, gaConfig)
     // Enable testing using modular-magician/terraform-provider-google
     parentId = "${projectId}_MM_GA"
-    val buildConfigModularMagicianGa = BuildConfigurationForSinglePackage(packageName, functionPackageGa.getValue("path"), "Provider-Defined Functions (GA provider, MM upstream)", ProviderNameGa, parentId, ModularMagicianVCSRootGa_featureBranchProviderFunctions, sharedResourcesEmpty, gaConfig)
-
+    val buildConfigModularMagicianGa = BuildConfigurationForSinglePackage(packageName, functionPackageGa.getValue("path"), "Provider-Defined Functions (GA provider, MM upstream)", ProviderNameGa, parentId, ModularMagicianVCSRootGa, sharedResourcesEmpty, vcrConfig)
 
     // Beta
     val betaConfig = getBetaAcceptanceTestConfig(allConfig)
     val functionPackageBeta = PackagesListBeta.getValue("functions")
-
     // Enable testing using hashicorp/terraform-provider-google-beta
     parentId = "${projectId}_HC_BETA"
     val buildConfigHashiCorpBeta = BuildConfigurationForSinglePackage(packageName, functionPackageBeta.getValue("path"), "Provider-Defined Functions (Beta provider, HashiCorp downstream)", ProviderNameBeta, parentId, HashicorpVCSRootBeta_featureBranchProviderFunctions, sharedResourcesEmpty, betaConfig)
-
     // Enable testing using modular-magician/terraform-provider-google-beta
     parentId = "${projectId}_MM_BETA"
-    val buildConfigModularMagicianBeta = BuildConfigurationForSinglePackage(packageName, functionPackageBeta.getValue("path"), "Provider-Defined Functions (Beta provider, MM upstream)", ProviderNameBeta, parentId, ModularMagicianVCSRootBeta_featureBranchProviderFunctions, sharedResourcesEmpty, betaConfig)
+    val buildConfigModularMagicianBeta = BuildConfigurationForSinglePackage(packageName, functionPackageBeta.getValue("path"), "Provider-Defined Functions (Beta provider, MM upstream)", ProviderNameBeta, parentId, ModularMagicianVCSRootBeta, sharedResourcesEmpty, vcrConfig)
 
     return Project{
         id(projectId)
         name = featureBranchProviderFunctionsName
-        description = "Subproject for testing feature branch ${featureBranchProviderFunctionsName}"
+        description = "Subproject for testing feature branch $featureBranchProviderFunctionsName"
 
-        // Register all feature branch-specific VCS roots in the project
+        // Register feature branch-specific VCS roots in the project
         vcsRoot(HashicorpVCSRootGa_featureBranchProviderFunctions)
         vcsRoot(HashicorpVCSRootBeta_featureBranchProviderFunctions)
-        vcsRoot(ModularMagicianVCSRootGa_featureBranchProviderFunctions)
-        vcsRoot(ModularMagicianVCSRootBeta_featureBranchProviderFunctions)
 
         // Register all build configs in the project
         buildType(buildConfigHashiCorpGa)
